7cd6b30da91419687c468cd810e2986f96c37408
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-09-19 15:48:00 +0900

    3 files updated

---
+++ hello/pygametst02.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+import sys
+
+def main():
+    pygame.init() # 初期化
+    (w, h) = (648, 486)
+    (x, y) = (int(w/2), int(h/2))
+    pygame.display.set_mode((w, h), 0, 32)
+    screen = pygame.display.get_surface()
+    pygame.display.set_caption("Pygame Test") # ウィンドウの上の方に出てくるアレの指定
+    bg = pygame.image.load("hello/jra.jpg").convert_alpha() # 背景画像の指定
+    rect_bg = bg.get_rect() # 画像のサイズ取得？？だと思われる
+    player = pygame.image.load("hello/PHPSQL.jpg").convert_alpha() # キャラ画像の指定
+    rect_player = player.get_rect() # 謎
+    rect_player.center = (x, y) # キャラ座標
+
+    while(True):
+        screen.fill((0, 0, 0, 0)) # 背景色の指定。RGBのはず
+        screen.blit(bg, rect_bg) # 背景画像の描画
+        screen.blit(player, rect_player) # キャラの描画
+        pygame.time.wait(30) # 更新間隔。多分ミリ秒
+        pygame.display.update() # 画面更新
+
+        for event in pygame.event.get(): # 終了処理
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+if __name__ == "__main__":
+    main()
